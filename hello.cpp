b29074b4f13ef76df082094b8903711482fbccbc
Hrithik Sharma <hrithik237@gmail.com>
2019-10-04 12:46:51 +0530

    hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+#define ll long long int
+#define v vector
+#define MP make_pair
+#define F first
+#define S second
+#define M 1000000007
+#define endl "\n"
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);
+using namespace std;
+
+inline void print(v<ll >&a){
+	for(int i=0;i<a.size();i++)
+		cout<<a[i]<<" ";
+	cout<<endl;
+}
+inline void inp(v<ll >&a){
+	for(int i=0;i<a.size();i++){
+		cin>>a[i];
+	}
+}
+
+int main(){
+	fastio();
+	cout<<"Hello World\n";
+
+		
+return 0;
+}
